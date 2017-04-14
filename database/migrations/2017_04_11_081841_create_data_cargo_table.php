9d6def8a700e500e9b0f32cf565f7ac2d9f27c13
zhulinjie <zhulinjie_cool@126.com>
2017-04-14 11:53:34 +0800

    优化

--- database/migrations/2017_04_11_081841_create_data_cargo_table.php
+++ database/migrations/2017_04_11_081841_create_data_cargo_table.php
@@ -25,7 +25,7 @@ class CreateDataCargoTable extends Migration
             $table->json('cargo_thumbnail')->comment('货品缩略图:多个');
             $table->text('cargo_info')->comment('货品详情');
             $table->text('cargo_info')->comment('货品详情');
-            $table->tinyInteger('cargo_status')->default(1)->comment('商品状态 1:待售 2:上架 3:下架	默认为 1');
+            $table->tinyInteger('cargo_status')->default(1)->comment('货品状态 1:待售 2:上架 3:下架	默认为 1');
             $table->timestamps();
             $table->softDeletes()->comment('软删除');
         });
