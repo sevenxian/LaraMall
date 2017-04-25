bcbf4cd87057eebb4400a94f7c15dae25aec3409
zhangyuchao <824520413@qq.com>
2017-04-25 12:09:28 +0000

    商品收藏

--- app/Model/GoodsCollection.php
+++ app/Model/GoodsCollection.php
@@ -3,9 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GoodsCollection extends Model
 {
+
+    use SoftDeletes;
     /**
      * 收货地址表
      *
