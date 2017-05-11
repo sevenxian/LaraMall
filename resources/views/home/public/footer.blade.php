055395682e743ef8d599fe1a77cc61593e287f91
zhangyuchao <824520413@qq.com>
2017-05-11 01:31:15 +0000

    basicconfig modified second

--- resources/views/home/public/footer.blade.php
+++ resources/views/home/public/footer.blade.php
@@ -17,7 +17,7 @@
             <a href="#">合作伙伴</a>
             <a href="#">联系我们</a>
             <a href="#">网站地图</a>
-            <em>{{$BasicConfig->getBasicConfig()->copyright}}</em>
+            <em>@if(empty($BasicConfig->getBasicConfig()->copyright))版权所有 @else {{$BasicConfig->getBasicConfig()->copyright}} @endif</em>
         </p>
     </div>
 </div>
