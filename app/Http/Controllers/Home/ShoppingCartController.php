415f929d6d1762eeb8d1e67da446aa4043cee729
zhulinjie <zhulinjie_cool@126.com>
2017-05-15 02:06:58 +0000

    优化购物车

--- app/Http/Controllers/Home/ShoppingCartController.php
+++ app/Http/Controllers/Home/ShoppingCartController.php
@@ -106,62 +106,9 @@ class ShoppingCartController extends Controller
         // 获取用户ID
         $userId = \Session::get('user')->user_id;
 
-        // 获取正在进行的活动
-        $currentTimestamp = time();
-        $activity = $this->activity->ongoingActivities($currentTimestamp);
-
         // 先从缓存中获取购物车信息
         $listLength = \Redis::lLen($this->listShoppingCart . $userId);
-        if (!$listLength) {
-            // 缓存中不存在则从数据库中获取
-            $carts = $this->cart->select(['user_id' => $userId]);
-            if (empty($carts)) {
-                return responseMsg(['data' => []]);
-            }
-
-            foreach ($carts as $val){
-                $cart = $val->toArray();
-
-                // 写入redis缓存
-                \Redis::rpush($this->listShoppingCart . $userId, $this->hashShoppingCart . $val->user_id . ':' . $val->cargo_id);
-
-                // 获取货品信息
-                $cargo = $this->cargo->find(['id' => $val->cargo_id])->toArray();
-
-                if(!$cargo){
-                    return responseMsg('货品不存在', 404);
-                }
-
-                // 活动正在进行中
-                if ($activity) {
-                    $activity->cargoActivity = $this->relGoodsActivity->find(['cargo_id' => $cargo['id'], 'activity_id' => $activity->id]);
-                }
-
-                if($activity && $activity->cargoActivity){
-                    // 活动价
-                    if($cart['shopping_number'] <= $activity->cargoActivity->number){
-                        \Redis::hmset($this->hashShoppingCart . $userId . ':' . $cargo['id'], ['user_id' => $cart['user_id'], 'cargo_id' => $cart['cargo_id'], 'shopping_number' => $cart['shopping_number'], 'price' => $activity->cargoActivity->promotion_price]);
-                        $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargo['id']], ['price' => $activity->cargoActivity->promotion_price]);
-                        $cart['price'] = $activity->cargoActivity->promotion_price;
-                    }else{
-                        \Redis::hmset($this->hashShoppingCart . $userId . ':' . $cargo['id'], ['user_id' => $cart['user_id'], 'cargo_id' => $cart['cargo_id'], 'shopping_number' => $cart['shopping_number'], 'price' => $cargo['cargo_price']]);
-                        $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargo['id']], ['price' => $cargo['cargo_price']]);
-                        $cart['price'] = $cargo['cargo_price'];
-                    }
-                }else{
-                    \Redis::hmset($this->hashShoppingCart . $userId . ':' . $cargo['id'], ['user_id' => $cart['user_id'], 'cargo_id' => $cart['cargo_id'], 'shopping_number' => $cart['shopping_number'], 'price' => $cargo['cargo_price']]);
-                    $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargo['id']], ['price' => $cargo['cargo_price']]);
-                    $cart['price'] = $cargo['cargo_price'];
-                }
-
-                if($cargo['inventory'] == 0){
-                    $cargo['shopping_number'] = 0;
-                }
-                
-                // 组装数据
-                $data[] = array_merge($cart, $this->standard($cargo));
-            }
-        }else{
+        if ($listLength) {
             // 获取hash的KEY便于取出所有商品
             $keys = \Redis::lrange($this->listShoppingCart . $userId, 0, $listLength);
             if (empty($keys)) {
@@ -172,48 +119,55 @@ class ShoppingCartController extends Controller
             foreach ($keys as $key => $item) {
                 // 从缓存中获取购物车信息
                 $cart = \Redis::hgetAll($item);
-                if(empty($cart)){
-                    $itemArr = explode(':', $item);
-                    $cart = $this->cart->find(['user_id' => $itemArr[4], 'cargo_id' => $itemArr[5]])->toArray();
-                    \Redis::hmset($this->hashShoppingCart . $cart['user_id'] . ':' . $cart['cargo_id'], ['user_id' => $cart['user_id'], 'cargo_id' => $cart['cargo_id'], 'shopping_number' => $cart['shopping_number'], 'price' => $cart['price']]);
-                }
-                
-                // 获取货品信息
-                $cargo = \Redis::hgetall($this->hashCargoInfo.$cart['cargo_id']);
 
-                if(empty($cargo)){
+                // 获取货品信息，先从redis里面获取，如果redis中没有缓存再从数据库中获取并写入到缓存
+                $cargo = \Redis::hgetall($this->hashCargoInfo . $cart['cargo_id']);
+                if (empty($cargo)) {
                     $cargo = $this->cargo->find(['id' => $cart['cargo_id']])->toArray();
-                    \Redis::hmset($this->hashCargoInfo . $cart['cargo_id'], $cargo);
+                    if (empty($cargo)) {
+                        responseMsg('非法操作', 400);
+                    }
+                    if (!\Redis::hmset(HASH_CARGO_INFO_ . $cart['cargo_id'], $cargo)) {
+                        responseMsg('货品信息写入缓存失败', 400);
+                    }
                 }
 
+                // 获取正在进行的活动
+                $currentTimestamp = time();
+                $activity = $this->activity->ongoingActivities($currentTimestamp);
                 // 货品正在做活动并且购买数量大于用来做活动的货品数量则不可以享受活动价
                 if ($activity) {
                     $activity->cargoActivity = $this->relGoodsActivity->find(['cargo_id' => $cargo['id'], 'activity_id' => $activity->id]);
                 }
-                if($activity && $activity->cargoActivity){
-                    if($cart['shopping_number'] <= $activity->cargoActivity->number){
+
+                if ($activity && $activity->cargoActivity) {
+                    if ($cart['shopping_number'] <= $activity->cargoActivity->number) {
                         // 活动价
                         $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargo['id'], 'price', $activity->cargoActivity->promotion_price);
-                        $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargo['id']], ['price' => $activity->cargoActivity->promotion_price]);
                         $cart['price'] = $activity->cargoActivity->promotion_price;
-                    }else{
+                    } else {
                         $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargo['id'], 'price', $cargo['cargo_price']);
-                        $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargo['id']], ['price' => $cargo['cargo_price']]);
                         $cart['price'] = $cargo['cargo_price'];
                     }
-                }else{
+                } else {
                     $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargo['id'], 'price', $cargo['cargo_price']);
-                    $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargo['id']], ['price' => $cargo['cargo_price']]);
                     $cart['price'] = $cargo['cargo_price'];
                 }
 
-                if($cargo['inventory'] == 0){
+                // 失败的情况 如果操作成功，哈希表中域字段已经存在且旧值已被新值覆盖则返回0
+                if ($res !== 0) {
+                    return responseMsg('修改购物车价格失败', 400);
+                }
+
+                if ($cargo['inventory'] == 0) {
                     $cargo['shopping_number'] = 0;
                 }
 
                 // 组装数据
                 $data[] = array_merge($cart, $this->standard($cargo));
             }
+        } else {
+            $data = [];
         }
 
         // 返回视图
@@ -291,7 +245,7 @@ class ShoppingCartController extends Controller
         }
 
         // 无库存的情况
-        if($cargo['inventory'] == 0){
+        if ($cargo['inventory'] == 0) {
             return responseMsg('该货品已被抢光', 414);
         }
 
@@ -312,12 +266,12 @@ class ShoppingCartController extends Controller
                 // 更改为活动价
                 if (($request['number'] + $cart['shopping_number']) <= $activity->cargoActivity->number) {
                     $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $activity->cargoActivity->promotion_price);
-                    $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $activity->cargoActivity->promotion_price]);
+                }else{
+                    $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $cargo['cargo_price']);
                 }
             } else {
                 // 更改为原价
                 $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $cargo['cargo_price']);
-                $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $cargo['cargo_price']]);
             }
 
             // 失败的情况 如果操作成功，哈希表中域字段已经存在且旧值已被新值覆盖则返回0
@@ -334,28 +288,16 @@ class ShoppingCartController extends Controller
             if ($activity && $activity->cargoActivity) {
                 // 更改为活动价
                 if ($request['number'] <= $activity->cargoActivity) {
-                    \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $activity->cargoActivity->promotion_price);
-                    $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $activity->cargoActivity->promotion_price]);
                     $data['price'] = $activity->cargoActivity->promotion_price;
-                }else{
-                    \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $cargo['cargo_price']);
-                    $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $cargo['cargo_price']]);
+                } else {
                     $data['price'] = $cargo['cargo_price'];
                 }
             } else {
-                \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $cargo['cargo_price']);
-                $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $cargo['cargo_price']]);
                 $data['price'] = $cargo['cargo_price'];
             }
 
             // 添加购买数量
             $data['shopping_number'] = $number;
-
-            // 存入数据库
-            $cart = $this->cart->insert($data);
-            if (!$cart) {
-                return responseMsg('加入购物车失败', 400);
-            }
 
             // 存入缓存
             $hashResult = \Redis::hMset($this->hashShoppingCart . $userId . ':' . $cargoId, $data);
@@ -438,7 +380,6 @@ class ShoppingCartController extends Controller
             }
             // 删除缓存中list记录
             \Redis::lRem($this->listShoppingCart . $userId, 0, $this->hashShoppingCart . $userId . ':' . $value);
-            $this->cart->delete(['user_id' => $userId, 'cargo_id' => $value]);
         }
 
         $cartLen = \Redis::lLen($this->listShoppingCart . $userId);
@@ -458,17 +399,22 @@ class ShoppingCartController extends Controller
     {
         // 获取用户ID
         $userId = \Session::get('user')->user_id;
+
         // 获取货品ID
         $cargoId = $request['cargoId'];
 
         // 获取货品信息
         $cargo = \Redis::hgetall(HASH_CARGO_INFO_ . $request['cargoId']);
-        if(empty($cargo)){
+        if (empty($cargo)) {
             $cargo = $this->cargo->find(['id' => $request['cargoId']])->toArray();
             if (empty($cargo)) {
                 return responseMsg('非法操作', 400);
             }
+            if (!\Redis::hmset(HASH_CARGO_INFO_ . $request['cargoId'], $cargo)) {
+                responseMsg('货品信息写入缓存失败', 400);
+            }
         }
+
         // 无货的情况
         if ($cargo['inventory'] == 0) {
             return responseMsg('该商品已被抢光', 414);
@@ -477,7 +423,6 @@ class ShoppingCartController extends Controller
         // 库存不足的情况
         if ($request['number'] > $cargo['inventory']) {
             \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'shopping_number', $cargo['inventory']);
-            $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['shopping_number' => $cargo['inventory']]);
             // 库存不充足
             return responseMsg($cargo['inventory'], 412);
         }
@@ -485,7 +430,6 @@ class ShoppingCartController extends Controller
         // 首先判断购买数量是否大于限定值
         if ($request['number'] > 200) {
             \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'shopping_number', 200);
-            $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['shopping_number' => 200]);
             return responseMsg('商品数量不能大于200', 410);
         }
 
@@ -499,34 +443,31 @@ class ShoppingCartController extends Controller
         // 货品正在做活动并且购买数量大于用来做活动的货品数量则不可以享受活动价
         if ($activity && $activity->cargoActivity) {
             $data['promotion_number'] = $activity->cargoActivity->number;
-            if ($request['number'] > $activity->cargoActivity->number) {
-                // 更改为原价
-                $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $cargo['cargo_price']);
-                $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $cargo['cargo_price']]);
-                $data['price'] = $cargo['cargo_price'];
-            } else {
-                // 更改为活动价
+            // 更改为活动价
+            if ($request['number'] <= $activity->cargoActivity->number) {
                 $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $activity->cargoActivity->promotion_price);
-                $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $activity->cargoActivity->promotion_price]);
                 $data['price'] = $activity->cargoActivity->promotion_price;
+            // 更改为原价
+            } else {
+                $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $cargo['cargo_price']);
+                $data['price'] = $cargo['cargo_price'];
             }
             // 失败的情况 如果操作成功，哈希表中域字段已经存在且旧值已被新值覆盖则返回0
-            if ($res != 0) {
+            if ($res !== 0) {
                 return responseMsg('操作失败!', 400);
             }
         } else {
             \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'price', $cargo['cargo_price']);
-            $this->cart->update(['user_id' => $userId, 'cargo_id' => $cargoId], ['price' => $cargo['cargo_price']]);
             $data['price'] = $cargo['cargo_price'];
         }
-
+        
         // 更新数量
         $res = \Redis::hset($this->hashShoppingCart . $userId . ':' . $cargoId, 'shopping_number', $request['number']);
 
-        if ($res != 0) {
+        if ($res !== 0) {
             return responseMsg('新增购买数量失败', 400);
         }
-        
+
         // 库存充足
         return responseMsg($data);
     }
