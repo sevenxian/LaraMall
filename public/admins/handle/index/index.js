d324c2376c323bbf9b7296d0b835634f95738bf0
zhangyuchao <824520413@qq.com>
2017-05-17 02:02:48 +0000

    完成后台首页统计

--- public/admins/handle/index/index.js
+++ public/admins/handle/index/index.js
@@ -2,14 +2,13 @@
  * 首页填充数据
  * @author zhangyuchao
  */
-new Vue({
+var EchartsData = new Vue({
     // 绑定元素
     el: '.wrapper',
     // 响应式参数
     data() {
         return {
-            datas: [], // 页码内容
-
+            header: [], // 数字统计
         }
     },
     // 第一次执行
@@ -24,8 +23,11 @@ new Vue({
             // 请求数据
             axios.post('/admin/index/count',{'_token':token}).then(response => {
                     if(response.data.ServerNo == 200){
-                        this.datas = response.data.ResultData.data;
-                        layer.closeAll();
+                        this.header = response.data.ResultData.header;
+                        // 注册方式统计饼图数据
+                        this.userCount(response.data.ResultData.main.user);
+                        // 订单成交率统计饼图数据
+                        this.orderCount(response.data.ResultData.main.order);
                     }
                 }).catch(error => {
                     // layer 加载层关闭
@@ -34,5 +36,86 @@ new Vue({
                 });
 
         },
+        // 注册方式统计
+        userCount(obj){
+            option = {
+                title : {
+                    text: '注册方式统计',
+                    subtext: 'LAMP兄弟连教学使用',
+                    x:'center'
+                },
+                tooltip : {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                    data: ['手机注册','邮箱注册']
+                },
+                series : [
+                    {
+                        name: '注册方式',
+                        type: 'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        data:[
+                            {value:obj.tel, name:'手机注册'},
+                            {value:obj.email, name:'邮箱注册'},
+                        ],
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            };
+
+            userChart.setOption(option);
+        },
+        // 订单成交率统计
+        orderCount(obj){
+            option = {
+                title : {
+                    text: '订单成交率统计',
+                    subtext: 'LAMP兄弟连教学使用',
+                    x:'center'
+                },
+                tooltip : {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                    data: ['未支付','支付超时','支付成功']
+                },
+                series : [
+                    {
+                        name: '订单成交率',
+                        type: 'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        data:[
+                            {value:obj.Unpaid, name:'未支付'},
+                            {value:obj.cancel, name:'支付超时'},
+                            {value:obj.payment, name:'支付成功'},
+                        ],
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            };
+
+            orderChart.setOption(option);
+        }
     }
 });
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,19 +20,8 @@
                             <i class="icon-user"></i>
                         </div>
                         <div class="value">
-                            <h1>@</h1>
-                            <p>用户总数量</p>
-                        </div>
-                    </section>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <section class="panel">
-                        <div class="symbol red">
-                            <i class="icon-tags"></i>
-                        </div>
-                        <div class="value">
-                            <h1>140</h1>
-                            <p>Sales</p>
+                            <h1>@{{ header.user }}</h1>
+                            <p>用户数量</p>
                         </div>
                     </section>
                 </div>
@@ -42,8 +31,19 @@
                             <i class="icon-shopping-cart"></i>
                         </div>
                         <div class="value">
-                            <h1>345</h1>
-                            <p>New Order</p>
+                            <h1>@{{ header.order }}</h1>
+                            <p>订单数量</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <section class="panel">
+                        <div class="symbol red">
+                            <i class="icon-tags"></i>
+                        </div>
+                        <div class="value">
+                            <h1>@{{ header.success_order }}</h1>
+                            <p>支付数量</p>
                         </div>
                     </section>
                 </div>
@@ -53,8 +53,8 @@
                             <i class="icon-bar-chart"></i>
                         </div>
                         <div class="value">
-                            <h1>34,500</h1>
-                            <p>Total Profit</p>
+                            <h1>@{{ header.price }}</h1>
+                            <p>收款金额</p>
                         </div>
                     </section>
                 </div>
@@ -79,53 +79,11 @@
 
 @section('customJs')
     <script> var token = "{{ csrf_token() }}" </script>
-    <script src="{{ asset('/admins/handle/index/index.js') }}"></script>
     <script src="{{ asset('/admins/js/echarts.common.min.js') }}"></script>
+    <script src="{{ asset('/admins/handle/index/index.js') }}"></script>
     <script type="text/javascript">
         // 基于准备好的dom，初始化echarts实例
         var userChart = echarts.init(document.getElementById('user'));
         var orderChart = echarts.init(document.getElementById('order'));
-
-        function getUser(){
-            option = {
-                title : {
-                    text: '注册方式统计',
-                    subtext: 'LAMP兄弟连教学使用',
-                    x:'center'
-                },
-                tooltip : {
-                    trigger: 'item',
-                    formatter: "{a} <br/>{b} : {c} ({d}%)"
-                },
-                legend: {
-                    orient: 'vertical',
-                    left: 'left',
-                    data: ['手机注册','邮箱注册']
-                },
-                series : [
-                    {
-                        name: '注册方式',
-                        type: 'pie',
-                        radius : '55%',
-                        center: ['50%', '60%'],
-                        data:[
-                            {value:35, name:'手机注册'},
-                            {value:66, name:'邮箱注册'},
-                        ],
-                        itemStyle: {
-                            emphasis: {
-                                shadowBlur: 10,
-                                shadowOffsetX: 0,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)'
-                            }
-                        }
-                    }
-                ]
-            };
-
-            return option
-        }
-        userChart.setOption(getUser());
-        //orderChart.setOption(option);
     </script>
 @stop
